f44a106797dec38a1be147ca7845b41e7883486c
Oleksandr.Y <oleksandr.y.as@agiliumlabs.com>
2024-03-27 16:59:23 +0200

    add REFERRAL_BONUS key

--- src/events/events.constants.ts
+++ src/events/events.constants.ts
@@ -1,5 +1,6 @@
 export enum EventKey {
     BET_BULL = "BET_BULL",
     BET_BEAR = "BET_BEAR",
-    START_ROUND= "START_ROUND"
+    START_ROUND= "START_ROUND",
+    REFERRAL_BONUS = "REFERRAL_BONUS"
 }
